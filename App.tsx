9e1827d0c4bf65811167db90b296729f6b17a683
HernanRinaldi <87478164+HernanRinaldi@users.noreply.github.com>
2023-02-01 22:33:49 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,17 @@
+
+ import 'react-native-gesture-handler';
+
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { Navigation } from './src/Navigation/Navigation';
+// import { Navigation } from './src/navigation/Navigation';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+        <Navigation />
+    </NavigationContainer>
+  )
+}
+
+export default App;
